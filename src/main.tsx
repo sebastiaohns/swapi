d6d3c516808d2bec555755b39c1c50e8c7cd4a95
sebastiaohns <sebastiaohns@gmail.com>
2026-07-10 08:15:49 -0300

    feat: Improve queries

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { router } from "./router/routes";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 5,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
