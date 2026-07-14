2c6bd87782ef85506a818a8afc784233d23a6bb8
sebastiaohns <sebastiaohns@gmail.com>
2026-07-14 08:10:59 -0300

    feat: Creating tests for film components

--- src/schemas/films.ts
+++ src/schemas/films.ts
@@ -24,3 +24,26 @@ export interface FilmDetailsType {
 }
 
 export type FilmType = DataType<FilmProperties>;
+
+export const SAMPLE_FILMS: FilmType[] = [
+  {
+    uid: "1",
+    description: "It is a dark time for the Rebellion.",
+    properties: {
+      created: "2026-07-14T02:00:28.148Z",
+      edited: "2026-07-14T02:00:28.148Z",
+      starships: ["https://www.swapi.tech/api/starships/2"],
+      vehicles: ["https://www.swapi.tech/api/vehicles/4"],
+      planets: ["https://www.swapi.tech/api/planets/1"],
+      producer: "Gary Kurtz, Rick McCallum",
+      title: "A New Hope",
+      episode_id: 4,
+      director: "George Lucas",
+      release_date: "1977-05-25",
+      opening_crawl: "It is a period of civil war.",
+      characters: ["https://www.swapi.tech/api/people/1"],
+      species: ["https://www.swapi.tech/api/species/1"],
+      url: "https://www.swapi.tech/api/films/1",
+    },
+  },
+];
